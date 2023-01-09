40cd16ec8962571c697f1db68fc4d65849427b27
VladDyadenko <Stilmagroup@gmail.com>
2023-01-09 21:11:50 +0200

    hw-8-10

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,11 +6,17 @@ console.log(galleryItems);
 
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import templateFunction from "../templates/templateItem.hbs" ;
+
 const  listImag = document.querySelector(".gallery");
 
 const addImagesToList = galleryItems
-.map(templateFunction)
+.map(({preview, original, description}) => `<a class="gallery__item" href="${original}">
+<img
+  class="gallery__image"
+  src="${preview}"
+  alt="${description}"
+/>
+</a>`)
 .join("");
 
 listImag.insertAdjacentHTML('beforeend', addImagesToList);
