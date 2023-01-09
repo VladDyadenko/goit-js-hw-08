c6048da97ca2f7397e9c1fa920cb1791869b0c11
VladDyadenko <Stilmagroup@gmail.com>
2023-01-09 17:00:11 +0200

    hw-8-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeEl = document.getElementById("vimeo-player")
+
+const player = new Player(iframeEl);
+
+player.on('timeupdate', throttle(onTime,1000))
+
+function onTime ({seconds}) {
+    localStorage.setItem("videoplayer-current-time", seconds)
+}
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
