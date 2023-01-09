915ede59f860fa40c104d3b56255fa57cff83d32
VladDyadenko <Stilmagroup@gmail.com>
2023-01-09 16:45:02 +0200

    hw-8-1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,31 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const  listImag = document.querySelector(".gallery");
+
+const addImagesToList = galleryItems
+.map(({preview, original, description}) => `<a class="gallery__item" href="${original}">
+<img
+  class="gallery__image"
+  src="${preview}"
+  alt="${description}"
+/>
+</a>`)
+.join("");
+
+listImag.insertAdjacentHTML('beforeend', addImagesToList);
+
+
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+    
+});
