59d9c7b5dfe323fdba9b93108d378e90f57a2b26
VladDyadenko <Stilmagroup@gmail.com>
2023-01-11 11:33:58 +0200

    hw-8 -remove mistake

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,11 +4,30 @@ import throttle from 'lodash.throttle';
 const iframeEl = document.getElementById("vimeo-player")
 
 const player = new Player(iframeEl);
+const VIMEO_KEY = "videoplayer-current-time";
+
+loadTime();
 
 player.on('timeupdate', throttle(onTime,1000))
 
 function onTime ({seconds}) {
-    localStorage.setItem("videoplayer-current-time", seconds)
+    localStorage.setItem(VIMEO_KEY, seconds)
 }
 
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
+function getSaveTime(){
+    return JSON.parse(localStorage.getItem(VIMEO_KEY)) || 0;
+    
+}
+
+function loadTime (){
+    const currentLoadTime = getSaveTime();
+    console.log(currentLoadTime);
+
+    player.setCurrentTime(currentLoadTime);
+    
+}
+
+
+
+
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,10 +3,9 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector("form");
 
 const STORAGE_KAY = "feedback-form-state";
-const dataForm = {};
-openFormValue ();
 
-
+let  dataForm = {};
+saveFromData()
 
 form.addEventListener('input', function (e) {
         
@@ -15,6 +14,7 @@ form.addEventListener('input', function (e) {
     
 
 });
+
 
 
 form.addEventListener('submit', throttle(onFormSubmit,500));
@@ -29,19 +29,64 @@ function onFormSubmit(e) {
 }
 
 
-function openFormValue () {
+function saveFromData() {
 
-    const saveDataValue = JSON.parse(localStorage.getItem(STORAGE_KAY));
-       
-    if( !saveDataValue  ){
+    let saveData = JSON.parse(localStorage.getItem(STORAGE_KAY));
+
+    if (saveData) {
+      try {
+        dataForm = saveData;
+
+        Object.entries(dataForm).forEach(
+          ([name, value]) => (form.elements[name].value = value)
+        );
         
-        return
-       
+      } catch (err) {
+        console.log(err.message);
+      }
     }
-   for(let key in saveDataValue){
-    form.elements[key].value = saveDataValue[key];
-   }
-    
-    
+  }
 
-}
+
+// import throttle from "lodash.throttle";
+
+// const refForm = document.querySelector(".feedback-form");
+
+// let dataForm = {};
+// const FORM_CURRENT_VALUE = "feedback-form-state";
+
+// refForm.addEventListener("submit", handleFormSubmit);
+// refForm.addEventListener("input", throttle(handleFormInputValue, 500));
+// getFormDataFromStorage();
+
+// function handleFormSubmit(event) {
+//   event.preventDefault();
+//   const formLength = event.currentTarget.elements.length - 1;
+//   if (Object.keys(dataForm).length !== formLength) {
+//     alert("Fill all fields");
+//     return;
+//   }
+//   event.currentTarget.reset();
+//   localStorage.removeItem(FORM_CURRENT_VALUE);
+//   console.log(dataForm);
+//   dataForm = {};
+// }
+// function handleFormInputValue(event) {
+//   dataForm[event.target.name] = event.target.value.trim();
+//   const stringifyFormData = JSON.stringify(dataForm);
+//   localStorage.setItem(FORM_CURRENT_VALUE, stringifyFormData);
+// }
+
+// function getFormDataFromStorage() {
+//   const savedMessage = localStorage.getItem(FORM_CURRENT_VALUE);
+//   if (savedMessage) {
+//     try {
+//       dataForm = JSON.parse(savedMessage);
+//       Object.entries(dataForm).forEach(
+//         ([name, value]) => (refForm.elements[name].value = value)
+//       );
+//     } catch (err) {
+//       console.log(err.message);
+//     }
+//   }
+// }
