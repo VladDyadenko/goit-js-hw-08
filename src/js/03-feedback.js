ce769567faabf142c1f07f59c8f07137b78d582e
VladDyadenko <Stilmagroup@gmail.com>
2023-01-11 16:26:55 +0200

    hw-8-11

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,6 +5,7 @@ const form = document.querySelector("form");
 const STORAGE_KAY = "feedback-form-state";
 
 let  dataForm = {};
+
 saveFromData()
 
 form.addEventListener('input', function (e) {
@@ -36,11 +37,12 @@ function saveFromData() {
     if (saveData) {
       try {
         dataForm = saveData;
-
+        console.log(dataForm);
         Object.entries(dataForm).forEach(
           ([name, value]) => (form.elements[name].value = value)
+       
         );
-        
+
       } catch (err) {
         console.log(err.message);
       }
@@ -48,45 +50,3 @@ function saveFromData() {
   }
 
 
-// import throttle from "lodash.throttle";
-
-// const refForm = document.querySelector(".feedback-form");
-
-// let dataForm = {};
-// const FORM_CURRENT_VALUE = "feedback-form-state";
-
-// refForm.addEventListener("submit", handleFormSubmit);
-// refForm.addEventListener("input", throttle(handleFormInputValue, 500));
-// getFormDataFromStorage();
-
-// function handleFormSubmit(event) {
-//   event.preventDefault();
-//   const formLength = event.currentTarget.elements.length - 1;
-//   if (Object.keys(dataForm).length !== formLength) {
-//     alert("Fill all fields");
-//     return;
-//   }
-//   event.currentTarget.reset();
-//   localStorage.removeItem(FORM_CURRENT_VALUE);
-//   console.log(dataForm);
-//   dataForm = {};
-// }
-// function handleFormInputValue(event) {
-//   dataForm[event.target.name] = event.target.value.trim();
-//   const stringifyFormData = JSON.stringify(dataForm);
-//   localStorage.setItem(FORM_CURRENT_VALUE, stringifyFormData);
-// }
-
-// function getFormDataFromStorage() {
-//   const savedMessage = localStorage.getItem(FORM_CURRENT_VALUE);
-//   if (savedMessage) {
-//     try {
-//       dataForm = JSON.parse(savedMessage);
-//       Object.entries(dataForm).forEach(
-//         ([name, value]) => (refForm.elements[name].value = value)
-//       );
-//     } catch (err) {
-//       console.log(err.message);
-//     }
-//   }
-// }
